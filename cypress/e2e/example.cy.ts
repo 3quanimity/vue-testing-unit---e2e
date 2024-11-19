e6ba1f1c4f509637774873b1482de3bcf3c07d30
Haythem <sinnner6@gmail.com>
2024-11-19 21:29:14 +0100

    cypress e2e testing

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,20 @@
 // https://on.cypress.io/api
 
 describe('My First Test', () => {
-  it('visits the app root url', () => {
+ 
+  beforeEach(()=>{
     cy.visit('/')
+  })
+
+  it('visits the app root url', () => {
     cy.contains('h1', 'You did it!')
+  })
+
+  it('smth', () => {
+    cy.get('.item').should('have.length', 5)
+
+    cy.get('.item .details h3').first().should('have.text', 'Documentation')
+
+    cy.get('.item .details h3').last().should('have.text', 'Support Vue')
   })
 })
